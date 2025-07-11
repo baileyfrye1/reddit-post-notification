f7d1ce8325816db5b7f63ec5530f57e0cb4a0d29
Bailey Frye <baileyafrye@comcast.net>
2025-07-10 23:16:50 -0500

    Got reddit scraper working. Need to get Discord DMs working

--- discord_setup.py
+++ discord_setup.py
@@ -1,5 +1,6 @@
-import os
 import logging
+import os
+
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -8,50 +9,25 @@ load_dotenv()
 
 discord_token = os.getenv("DISCORD_TOKEN")
 
+
+# Convert below to a class for more OOP approach and for better usability in other files
+class DiscordClient(discord.Client):
+    async def on_ready(self):
+        print(f"We are ready to start, {self.user}")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+
 # Set up Discord
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-
-@bot.event
-async def on_ready():
-    print(f"We are ready to start, {bot.user.name}")
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    await bot.process_commands(message)
-
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f"Hello {ctx.author.mention}!")
-
-
-@bot.command()
-async def dm(ctx, *, msg):
-    await ctx.author.send(f"You said '{msg}'")
-
-
-@bot.command()
-async def reply(ctx):
-    await ctx.reply("This is a new reply to your message")
-
-
-@bot.command()
-async def poll(ctx, *, question):
-    embed = discord.Embed(title="New Poll", description=question)
-    poll_message = await ctx.send(embed=embed)
-    await poll_message.add_reaction("👍")
-    await poll_message.add_reaction("👎")
-
-
+client = DiscordClient(intents=intents)
 if discord_token is not None:
-    bot.run(discord_token, log_handler=handler, log_level=logging.DEBUG)
+    client.run(discord_token, log_handler=handler, log_level=logging.DEBUG)
+
+user = client.get_user(1127378884564897873)
--- main.py
+++ main.py
@@ -1,34 +1,29 @@
+import asyncio
 import os
-import praw
-import discord_setup
+
+import asyncpraw as praw
 from dotenv import load_dotenv
+
+from discord_setup import user
 
 load_dotenv()
 
-# Set up Reddit
+# Initilize Reddit instance
 reddit = praw.Reddit(
     client_id=os.getenv("REDDIT_CLIENT_ID"),
     client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-    password=os.getenv("REDDIT_PASSWORD"),
     user_agent="Reddit Post Notification Bot",
-    username=os.getenv("REDDIT_USERNAME"),
 )
 
-# Set up variables
-SUBREDDIT = os.getenv("SUBREDDIT")
-INTERVAL = os.getenv("INTERVAL")
 
-# while True:
-#     # Get new posts
-#     new_posts = reddit.subreddit(SUBREDDIT).new(limit=100)
-#
-#     # Check if new posts exist
-#     if new_posts:
-#         # Loop through new posts
-#         for post in new_posts:
-#             # Check if post title matches
-#             if post.title == POST_TITLE:
-#                 # Check if post body matches
-#                 if post.selftext == POST_BODY:
-#                     # Sleep for interval
-#                     time.sleep(int(INTERVAL))
+async def send_message():
+    subreddit = await reddit.subreddit("fantasyromance")
+    for submission in subreddit.stream.submissions():
+        if "Editor Read" in submission.title:
+            await user.send(
+                f"New submission from XusBookReviews: {submission.title}\nhttps://reddit.com{submission.peramlink}"
+            )
+
+
+if __name__ == "__main__":
+    asyncio.run(send_message())
