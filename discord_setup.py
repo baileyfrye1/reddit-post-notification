27979523ef33580c8ca20b1ce0cd563176d6b466
Bailey Frye <baileyafrye@comcast.net>
2025-07-12 20:50:44 -0500

    Initial working version of Reddit to Discord bot

--- discord_setup.py
+++ discord_setup.py
@@ -1,23 +1,43 @@
+import asyncio
 import logging
 import os
 
 import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 discord_token = os.getenv("DISCORD_TOKEN")
+user_token = os.getenv("DISCORD_USER_TOKEN")
+channel_id = os.getenv("DISCORD_CHANNEL_ID")
+
+ready_event = asyncio.Event()
 
 
 # Convert below to a class for more OOP approach and for better usability in other files
 class DiscordClient(discord.Client):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.channel = None
+
     async def on_ready(self):
         print(f"We are ready to start, {self.user}")
 
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
+        self.channel = self.get_channel(int(channel_id))
+
+        if self.channel is None:
+            print(f"Failed to find channel with id: {channel_id}")
+        else:
+            print("Ready to send messages")
+
+        ready_event.set()
+
+    async def send_message(self, text: str):
+        await ready_event.wait()
+        if self.channel:
+            await self.channel.send(text)
+        else:
+            print("Channel not found")
 
 
 # Set up Discord
@@ -25,9 +45,11 @@ handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w"
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.guilds = True
 
 client = DiscordClient(intents=intents)
-if discord_token is not None:
-    client.run(discord_token, log_handler=handler, log_level=logging.DEBUG)
 
-user = client.get_user(1127378884564897873)
+
+async def start_discord():
+    if discord_token:
+        await client.start(discord_token)
--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import asyncio
 import os
 
-import asyncpraw as praw
+import asyncpraw
 from dotenv import load_dotenv
 
-from discord_setup import user
+from discord_setup import client, start_discord
 
 load_dotenv()
 
-# Initilize Reddit instance
-reddit = praw.Reddit(
-    client_id=os.getenv("REDDIT_CLIENT_ID"),
-    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-    user_agent="Reddit Post Notification Bot",
-)
 
-
-async def send_message():
+async def monitor_reddit():
+    reddit = asyncpraw.Reddit(
+        client_id=os.getenv("REDDIT_CLIENT_ID"),
+        client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+        user_agent="Reddit Post Notification Bot",
+    )
     subreddit = await reddit.subreddit("fantasyromance")
-    for submission in subreddit.stream.submissions():
+
+    async for submission in subreddit.stream.submissions():
         if "Editor Read" in submission.title:
-            await user.send(
-                f"New submission from XusBookReviews: {submission.title}\nhttps://reddit.com{submission.peramlink}"
+            await client.send_message(
+                f"New submission from XusBookReviews: {submission.title}\nhttps://reddit.com{submission.permalink}"
             )
 
 
+async def main():
+    await asyncio.gather(start_discord(), monitor_reddit())
+
+
 if __name__ == "__main__":
-    asyncio.run(send_message())
+    asyncio.run(main())
